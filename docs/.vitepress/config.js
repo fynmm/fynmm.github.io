dd638fc9e1488164e1a1b3058c3f13293e0ad18c
HeartlessSoy <luhainan@111.com.cn>
2023-08-17 18:25:49 +0800

    创建博客工程

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,43 @@
+const primarySidebar = [
+  { text: 'Our Story', link: '/about/our-story' }
+]
+
+module.exports = {
+  title: '无情酱油的博客',
+  description: 'Just playing around.',
+  themeConfig: {
+    search: false,
+    searchMaxSuggestions: 10,
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    },
+    docFooter: {
+      prev: false,
+      next: false,
+    },
+    search: {
+
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { text: 'Contact', link: '/contact'},
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: {
+      '/about/': primarySidebar,
+      '/contact': primarySidebar,
+
+  }
+
+
+    // carbonAds: {
+      // code: 'CEBDT27Y',
+      // placement: 'vuejsorg'
+    // }
+  }
+}
+
+
+//  http://www.dtmao.cc/Html5/111900.html
---
+++ docs/index.md
@@ -0,0 +1,46 @@
+<!-- ---
+home: true
+
+
+sidebar: false
+next: false
+---
+# Home -->
+
+
+<!-- ## Routing
+
+[docs/index.md](/) -> /
+
+[docs/contact.md](/contact) -> /contact
+
+[about/index.md](/about/) -> /about/
+
+[about/index.md](/about/our-story) -> /about/our-stoyr
+
+
+### All these options work!
+[docs/contact](/contact) |
+[docs/contact.md](/contact.md) |
+[docs/contact.html](/contact.html) 
+
+
+
+``` js
+ abc
+``` -->
+
+
+<!-- ```
+// .vitepress/config.js
+
+| Headings      | Are           | Centered    |
+| ------------- |:-------------:| -----:      |
+| left align    | centered      | right align |
+| zebra striped | rows          | easy        |
+```
+
+| Headings      | Are           | Centered    |
+| ------------- |:-------------:| -----:      |
+| left align    | centered      | right align |
+| zebra striped | rows          | easy        | -->
